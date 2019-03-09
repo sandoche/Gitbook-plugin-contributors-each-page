3c0a9806c6d5ceb78a8b73a90aa5491a4bdb14eb
Sandoche Adittane <sandoche@protonmail.com>
2019-03-09 19:09:05 +0100

    :construction: WIP plugin

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
+var getContributors = require('github-contributors-file');
+
+function htmlContributor (avatarUrl, login, url) {
+  return `
+    <a style="float: left; display: inline-block;" href="${url}">
+      <img src="${avatarUrl}" height="30px" width="30px" alt="${login}"/>
+    </a>
+  `;
+}
+
 module.exports = {
   // Map of hooks
-  hooks: {},
+  hooks: {
+    "page": function(page) {
+      const owner = this.options.pluginsConfig['contributors-each-page'].owner;
+      const repository = this.options.pluginsConfig['contributors-each-page'].repository;
+      let htmlContributors = '';
 
-  // Map of new blocks
-  blocks: {},
+      getContributors(owner, repository, page.rawPath).then((contributors) => {
+        console.log(contributors);
+        
 
-  // Map of new filters
-  filters: {}
+        htmlContributors = contributors.reduce((value, item) => {
+          value = value + htmlContributor(item.avatar_url, item.login, item.url);
+        }, '');
+
+        console.log(htmlContributors);
+
+        page.content = page.content + htmlContributors;
+      });
+
+      return page;
+    }
+  }
 };
