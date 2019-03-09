fdf42ac7eefdf7d84edc36dfa8e20e0427420afa
Sandoche Adittane <sandoche@protonmail.com>
2019-03-09 20:20:39 +0100

    :construction: WIP everything should work, but cannot pass the token

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ global.fetch = require("node-fetch");
 
 function htmlContributor (avatarUrl, login, url) {
   return `
-    <a style="float: left; display: inline-block;" href="${url}">
-      <img src="${avatarUrl}" height="30px" width="30px" alt="${login}"/>
+    <a style="float: left; display: inline-block; margin-right: 4px" href="${url}">
+      <img src="${avatarUrl}" height="30px" width="30px" alt="${login}" target="_blank" style="border-radius: 15px;"/>
     </a>
   `;
 }
-
 module.exports = {
-  // Map of hooks
   hooks: {
     "page": function(page) {
       return new Promise((resolve, reject) => {
@@ -19,19 +17,17 @@ module.exports = {
         const repository = this.options.pluginsConfig['contributors-each-page'].repository;
         let htmlContributors = '';
 
-        getContributors(owner, repository, page.path).then((contributors) => {
-          console.log(contributors);
-          
-          // htmlContributors = contributors.reduce((value, item) => {
-          //   value = value + htmlContributor(item.avatar_url, item.login, item.url);
-          // }, '');
+        getContributors(owner, repository, page.path).then((contributors) => {         
+          contributorsInHtml = contributors.reduce((value, item) => {
+            return value + htmlContributor(item.avatar_url, item.login, item.url);
+          }, '');
 
-          // console.log(htmlContributors);
-
-          page.content = page.content + JSON.stringify(contributors);
+          page.content = page.content + '<div style="margin-top: 64px;">' + contributorsInHtml + '</div>';
           resolve(page);
         }).catch((e) => {
-          reject(page)
+          console.log('Contributors failed for page ' + page.path);
+          console.log(e);
+          resolve(page);
         });
       });
     }
